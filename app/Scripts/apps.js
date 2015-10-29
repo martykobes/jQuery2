2b07c3edbbd4c9ed3b41bc6acb297f8ccc60acd9
Marty Kowbel <mekobes44@yahoo.com>
2015-10-29 14:34:06 -0500

    updated listo

--- app/Scripts/apps.js
+++ app/Scripts/apps.js
@@ -1,11 +1,11 @@
-$(document).ready(function () {
+$(document).ready(function() {
     var listo = [];
-    var Task = function (task) {
+    var Task = function(task) {
         this.task = task;
         this.id = 'new';
     };
     $('#newTaskForm').hide();
-    var advanceTask = function (task) {
+    var advanceTask = function(task) {
         var modified = task.innerText.trim()
         for (var i = 0; i < listo.length; i++) {
             if (listo[i].task === modified) {
@@ -21,58 +21,59 @@ $(document).ready(function () {
         }
         task.remove();
     };
-    var addTask = function (task) {
+    var addTask = function(task) {
         if (task) {
-            task = new Task(task);
-            listo.push(task);
-            $('#newItemInput').val('');
-            $('#newList').append('<a href="#" class="" id="item"><li class="list-group-item">' + task.task + '<span class="arrow pull-right"><i class="glyphicon glyphicon-arrow-right"></span></li></a>');
+         task = new Task(task);
+         listo.push(task);
+         $('#newItemInput').val('');
+         $('#newList').append('<a href="#" class="" id="item"><li class="list-group-item">' + task.task + '<span class="arrow pull-right"><i class="glyphicon glyphicon-arrow-right"></span></li></a>');
 
-        }
-        $('#newTaskItem', '#newListItem').fadeToggle('fast', 'linear');
-        $('saveNewItem').on('click', function (e) {
-            e.preventDefault();
-            var task = $('#newItenInput').val().trim();
-            addTask(task);
-        })
-        $('#newListItem').on('click', function () {
-            $('#newTaskForm,  #newListItem').fadeToggle('fast', 'linear');
-        });
-        //closes form
-        $('#cancel').on('click', function (e) {
-            e.preventDefault();
-            $('#newTaskForm,  #newListItem').fadeToggle('fast', 'linear');
-        });
-        $(document).on('click', '#item', fukction(e) {
-            e.preventDefault();
-        });
-        $(document).on('click', '#item', function (e) {
-            e.preventDefault();
-            var task = this;
-            advanceTask(task);
-            this.id = 'inProgress';
-        });
-        $(document).on('click', '#item', function (e) {
-            e.preventDefault();
-            var task = this;
-            advanceTask(task);
-            this.id = 'inProgress';
-            $('#currentList').append(this.outerHTML);
-
-        });
-        $(document).on('click', '#inProgress', function (e) {
-            e.preventDefault();
-            var task = this;
-            task.id = "archived";
-            var changeIcon = task.outerHTML.replace('glyphicon-arrow-right', 'glyphicon-remove');
-            advanceTask(task);
-            $('#archivedList').append(changeIcon);
-        });
-        $(document).on('click', '#archived', function (e) {
-            e.preventDefault();
-            var task = this;
-            advanceTask(task);
-        });
+     }
+     $('#newTaskItem', '#newListItem').fadeToggle('fast', 'linear');
     };
+     $('#saveNewItem').on('click', function(e) {
+         e.preventDefault();
+         var task = $('#newItemInput').val().trim();
+         addTask(task);
+     })
+     $('#newListItem').on('click', function() {
+         $('#newTaskForm,  #newListItem').fadeToggle('fast', 'linear');
+     });
+     
+     $('#cancel').on('click', function(e) {
+         e.preventDefault();
+         $('#newTaskForm,  #newListItem').fadeToggle('fast', 'linear');
+     });
+     $(document).on('click', '#item', function(e) {
+         e.preventDefault();
+     });
+     $(document).on('click', '#item', function(e) {
+         e.preventDefault();
+         var task = this;
+         advanceTask(task);
+         this.id = 'inProgress';
+     });
+     $(document).on('click', '#item', function(e) {
+         e.preventDefault();
+         var task = this;
+         advanceTask(task);
+         this.id = 'inProgress';
+         $('#currentList').append(this.outerHTML);
 
-})
+     });
+     $(document).on('click', '#inProgress', function(e) {
+         e.preventDefault();
+         var task = this;
+         task.id = "archived";
+         var changeIcon = task.outerHTML.replace('glyphicon-arrow-right', 'glyphicon-remove');
+         advanceTask(task);
+         $('#archivedList').append(changeIcon);
+     });
+     $(document).on('click', '#archived', function(e) {
+     e.preventDefault();
+     var task = this;
+     advanceTask(task);
+     });
+     });
+
+  
